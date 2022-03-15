67a2fb6e193888de8283fc4b635ecfe0411ed889
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2022-03-15 17:10:14 +0100

    Implement help command

--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using WiredBrainCoffee.DataAccess;
+
 Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+
+Console.WriteLine("Write 'help' to list available commands");
+
+var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+while (true)
+{
+    var line = Console.ReadLine();
+
+    var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+    if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("> Available coffee shop commands:");
+        foreach (var coffeeShop in coffeeShops)
+        {
+            Console.WriteLine($"> " + coffeeShop.Location);
+        }
+    }
+}
